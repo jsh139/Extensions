4090925b85d38e321f4198cbe638d584a896721b
jsh139 <jsh139@gmail.com>
2014-09-18 11:18:13 -0400

    Firefox MehScroller
    Firefox MehScroller code

---
+++ Firefox_MehScroller/lib/main.js
@@ -0,0 +1,17 @@
+var self = require("sdk/self");
+
+var button = require("sdk/ui/button/action").ActionButton({
+    id: "Meh-Scroller",
+    label: "Scroll to next unread reply",
+    icon: {
+        "16": "./icon16.png",
+        "32": "./icon32.png",
+        "64": "./icon64.png"
+    },
+    onClick: function () {
+        console.log('Scrolling to next unread reply');
+        require("sdk/tabs").activeTab.attach({
+            contentScriptFile: self.data.url("replyScroll.js")
+        });
+    }
+});

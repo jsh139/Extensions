013b816bacff27efb79abc0bd44277e770684456
Josh Hoffman <josh.hoffman@sungardas.com>
2015-08-27 16:27:31 -0400

    v1.5 for Firefox
    Added PageMod and anonymous function wrap for Firefox

--- Firefox_MehScroller/data/replyScroll.js
+++ Firefox_MehScroller/data/replyScroll.js
@@ -5,47 +5,52 @@
 /*  Do not modify without permission. No warranty expressed or implied.                     */
 /********************************************************************************************/
 
-var unreadElements = $('.unread');
+(function () {
+    self.port.on("scroll", replyScroll);
 
-if (!$('#CommentCounter').length) {
-    $('<input>').attr({
-        type: 'hidden',
-        id: 'CommentCounter',
-        name: 'CommentCounter',
-        value: '-1'
-    }).appendTo('body');
+    function replyScroll() {
+        var unreadElements = $('.unread');
 
-    $('<div/>')
-        .attr({
-            id: 'MessageDiv',
-            style: 'background:#E2E5EC; padding:3px 8px; position: fixed; top: 10px; right: 100px; border: 1px solid #747474; border-radius: 5px; display:none; z-index: 9999;'
-        })
-        .append('<span/>')
-        .appendTo('body');
-}
+        if (!$('#CommentCounter').length) {
+            $('<input>').attr({
+                type: 'hidden',
+                id: 'CommentCounter',
+                name: 'CommentCounter',
+                value: '-1'
+            }).appendTo('body');
 
-var i = parseInt($('#CommentCounter').val());
+            $('<div/>')
+                .attr({
+                    id: 'MessageDiv',
+                    style: 'background:#E2E5EC; padding:3px 8px; position: fixed; top: 10px; right: 100px; border: 1px solid #747474; border-radius: 5px; display:none; z-index: 9999;'
+                })
+                .append('<span/>')
+                .appendTo('body');
+        }
 
-if (unreadElements.length > 0) {
-    /* Click the archive button in case there are unread replies in there */
-    if ($('.show-comments').is(':visible'))
-        $('.show-comments')[0].click();
+        var i = parseInt($('#CommentCounter').val());
 
-    if (i >= unreadElements.length - 1) {
-        i = -1;
-        DisplayMessage('Reached end of thread, scrolling to first unread');
+        if (unreadElements.length > 0) {
+            /* Click the archive button in case there are unread replies in there */
+            if ($('.show-comments').is(':visible'))
+                $('.show-comments')[0].click();
+
+            if (i >= unreadElements.length - 1) {
+                i = -1;
+                displayMessage('Reached end of thread, scrolling to first unread');
+            }
+
+            unreadElements[++i].scrollIntoView();
+            $('#CommentCounter').val(i.toString());
+        } else
+            displayMessage('There are no unread replies in this thread');
     }
 
-    unreadElements[++i].scrollIntoView();
-    $('#CommentCounter').val(i.toString());
-}
-else
-    DisplayMessage('There are no unread replies in this thread');
-
-function DisplayMessage(message) {
-    $('#MessageDiv')
-        .html('<span>' + message + '</span>')
-        .fadeIn(500, function () {
-            $('#MessageDiv').fadeOut(3000)
-        });
-}
+    function displayMessage(message) {
+        $('#MessageDiv')
+            .html('<span>' + message + '</span>')
+            .fadeIn(500, function() {
+                $('#MessageDiv').fadeOut(3000)
+            });
+    }
+})();
--- Firefox_MehScroller/lib/main.js
+++ Firefox_MehScroller/lib/main.js
@@ -1,4 +1,25 @@
-var self = require("sdk/self");
+var self = require("sdk/self").data;
+var tab = require("sdk/tabs");
+var pageMod = require("sdk/page-mod");
+var workers = [];
+
+pageMod.PageMod({
+    include: ["https://meh.com/forum*", "https://mediocre.com/forum*", "https://drone.horse/forum*"],
+    contentScriptFile: [self.url("jquery-2.1.4.min.js"), self.url("replyScroll.js")],
+    onAttach: function onAttach(worker) {
+        workers.push(worker);
+        worker.on('detach', function() {
+            detachWorker(this, workers);
+        });
+    }
+});
+
+function detachWorker(worker, workerArray) {
+    var index = workerArray.indexOf(worker);
+    if (index != -1) {
+        workerArray.splice(index, 1);
+    }
+}
 
 var button = require("sdk/ui/button/action").ActionButton({
     id: "Meh-Scroller",
@@ -9,9 +30,11 @@ var button = require("sdk/ui/button/action").ActionButton({
         "64": "./icon64.png"
     },
     onClick: function () {
-        console.log('Scrolling to next unread reply');
-        require("sdk/tabs").activeTab.attach({
-            contentScriptFile: [self.data.url("jquery-2.1.4.min.js"), self.data.url("replyScroll.js")]
-        });
+        for (var i = 0; i < workers.length; i++) {
+            if (workers[i].tab == tab.activeTab) {
+                console.log('Scrolling to next unread reply');
+                workers[i].port.emit("scroll");
+            }
+        }
     }
 });
